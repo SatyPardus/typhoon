8e406788b1b50419ede344be55583b159b55561b
superp00t <superp00t@tutanota.com>
2025-04-29 22:36:11 -0400

    feat(plane): add default constructor for C4Plane

--- tempest/plane/C4Plane.hpp
+++ tempest/plane/C4Plane.hpp
@@ -6,8 +6,10 @@
 class C4Plane {
     public:
     // Member variables
-    C3Vector n;
-    float d;
+    C3Vector n = { 0.0f, 0.0f, 1.0f };
+    float d = 0.0f;
+
+    C4Plane() = default;
 };
 
 #endif
